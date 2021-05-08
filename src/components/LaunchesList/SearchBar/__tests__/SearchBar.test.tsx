b8e0a30a958f0ace22180606e237fd42781bf33d
Rin <przemyslaw.k.szubert@gmail.com>
2021-05-08 17:48:39 +0200

    Added SearchBar tests

---
+++ src/components/LaunchesList/SearchBar/__tests__/SearchBar.test.tsx
@@ -0,0 +1,19 @@
+import {mount} from 'enzyme';
+import React from 'react';
+import SearchBar from '../index';
+
+it('mount without crashing', () => {
+  mount(<SearchBar onChange={() => {}}/>);
+});
+
+it('input triggers onChange event', () => {
+  const onChangeMock = jest.fn();
+  const event = {
+    target: { value: 'abcd' }
+  };
+  const searchBar = mount(<SearchBar onChange={onChangeMock}/>);
+
+  searchBar.find('input').simulate('change', event);
+
+  expect(onChangeMock).toBeCalledWith('abcd');
+})
